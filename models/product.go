0752c773d0a9992f1b8ebedba083446e63dd28c5
Muhamad Andre <andrepriyanto95@gmail.com>
2023-11-06 20:40:50 +0700

    add hooks

--- models/product.go
+++ models/product.go
@@ -1,12 +1,26 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Product struct {
 	gorm.Model
 
+	ID           string `json:"id"`
 	ProductName  string `json:"product_name"`
 	Type         string `json:"type"`
 	ProductImage string `json:"product_image"`
 	Description  string `json:"description"`
+}
+
+func (p *Product) BeforeCreate(tx *gorm.DB) (err error) {
+	p.ID = uuid.New().String()
+	p.CreatedAt = time.Now().Local().UTC()
+	p.UpdatedAt = time.Now().Local().UTC()
+
+	return
 }
