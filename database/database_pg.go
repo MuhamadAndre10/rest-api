133d1b6b923e2055c7f3d57345f0a21fd8728589
Muhamad Andre <andrepriyanto95@gmail.com>
2023-11-06 15:02:25 +0700

    make a open connection

--- database/database_pg.go
+++ database/database_pg.go
@@ -1,1 +1,41 @@
 package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	DB *gorm.DB
+}
+
+// make variable global for access database instance
+var Db DbInstance
+
+func ConnecDb() {
+
+	dsn := "host=localhost user=postgres password='root' dbname=product_db port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("failed to connect to database. \n", err)
+	}
+
+	log.Println("connected")
+
+	db.Logger = logger.Default.LogMode(logger.Info)
+
+	log.Println("running migration")
+	db.AutoMigrate()
+
+	Db = DbInstance{
+		DB: db,
+	}
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
 func main() {
+	// make connection to connect postgres database.
+
+	// instance a new fiber
+
+	//set up middleware, if needed
+
+	// make a route and handlers
+
+	// make a connection server.
 
 }
